cf58ecacdc3212fd1ff0e6b80dafa521505d9bf6
Jeremy Stanley <fungi@yuggoth.org>
2015-04-28 19:48:55 +0000

    Add positive/negative tests exercising the parser
    Change-Id: I825200db7dfa336741481d39b4b294818cf30057

--- bindep/tests/test_depends.py
+++ bindep/tests/test_depends.py
@@ -19,6 +19,7 @@ import subprocess
 from textwrap import dedent
 
 import mox
+import ometa.runtime
 from testtools.matchers import Contains
 from testtools.matchers import Equals
 from testtools.matchers import MatchesSetwise
@@ -183,6 +184,17 @@ class TestDepends(TestCase):
         self.assertEqual(
             [('badversion', [('foo', "!=123", "123")])],
             depends.check_rules([("foo", [], [("!=", "123")])]))
+
+    def test_parser_patterns(self):
+        Depends(dedent("""\
+            foo
+            bar [something]
+            quux [anotherthing !nothing] <=12
+            """))
+
+    def test_parser_invalid(self):
+        self.assertRaises(ometa.runtime.ParseError,
+                          lambda: Depends("foo [platform:bar@baz]\n"))
 
 
 class TestDpkg(TestCase):
